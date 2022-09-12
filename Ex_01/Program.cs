44b57f974dbbd8631386fb38d836a02920d647c7
«Svetlana <svdorohova@gmail.com>
2022-09-12 10:38:07 +0300

    Add ex01

---
+++ Ex_01/Program.cs
@@ -0,0 +1,48 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+double[,] NewRandomMas(int rows, int columns)
+{
+    double[,] mas = new double[rows, columns];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().Next(-10, 10);
+        }
+    }
+    for (int i = 0; i < mas.GetLength(0); i=i+2)
+    {
+        for (int j = 0; j < mas.GetLength(1); j=j+2)
+        {
+            mas[i, j] = mas[i, j] /10;
+        }
+    }
+    return mas;
+}
+
+void PrintArray(double[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int rows = 3;
+int columns = 4;
+double[,] randmas = NewRandomMas(rows, columns);
+PrintArray (randmas);
+
+
