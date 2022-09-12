476751e6ef8af2dc7a83b12e2392a2595aa8949c
«Svetlana <svdorohova@gmail.com>
2022-09-12 13:16:49 +0300

    Вторая задача в двух вариациях

---
+++ Ex_02(1)/Program.cs
@@ -0,0 +1,54 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[] NumberPosition(string a)
+{
+    a = a.Replace("(", "");
+    a = a.Replace(")", "");
+    string[] mas1 = a.Split(",");
+    int[] mas = new int[mas1.Length];
+    for (int i = 0; i < mas1.Length; i++)
+    {
+        mas[i] = Convert.ToInt32(mas1[i]);
+    }
+    return mas;
+}
+
+void PrintValuePosition(int[,] mas, int[] given)
+{
+    if (given[0] > 0 && given[1] > 0 && given[0] <= mas.GetLength(0) && given[1] <= mas.GetLength(1))
+    {
+
+        int i = given[0] - 1;
+        int j = given[1] - 1;
+        Console.WriteLine($"Значение в массиве равно: {mas[i, j]}.");
+
+    }
+    else
+    {
+        Console.WriteLine("Такой позиции не существует в матрице.");
+    }
+}
+
+void PrintArray(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] givenArray = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+System.Console.WriteLine("Введите позицию элемента в матрице через запятую");
+PrintValuePosition(givenArray, NumberPosition(Console.ReadLine()));
+System.Console.WriteLine();
+PrintArray(givenArray);
---
+++ Ex_02/Program.cs
@@ -0,0 +1,73 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+//  и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 17 -> такого числа в массиве нет
+
+int[,] NewRandomMas(int rows, int columns)
+{
+    int[,] mas = new int[rows, columns];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            mas[i, j] = new Random().Next(0, 16);
+        }
+    }
+    return mas;
+}
+
+int[] CheckMas(int[,] mas, int value)
+{
+    int[] desired = new int[2];
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            if (mas[i, j] == value)
+            {
+                desired[0] = i + 1;
+                desired[1] = j + 1;
+            }
+        }
+    }
+    return desired;
+}
+
+void PrintArray(int[,] mas)
+{
+    for (int i = 0; i < mas.GetLength(0); i++)
+    {
+        for (int j = 0; j < mas.GetLength(1); j++)
+        {
+            Console.Write($"{mas[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Print(int[] mas)
+{
+    if (mas[0] == 0 || mas[1] == 0)
+    {
+        System.Console.Write("Такого числа в массиве нет");
+    }
+    else
+    {
+        for (int i = 0; i < 1; i++)
+        {
+            for (int j = 1; j < mas.Length; j++)
+            {
+                System.Console.Write($"Позиция элемента в массиве: ({mas[i]}, {mas[j]})");
+            }
+        }
+    }
+}
+
+System.Console.WriteLine("Введите значение числа в массиве: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int[,] valuuemas = NewRandomMas(3, 4);
+PrintArray(valuuemas);
+Print(CheckMas(valuuemas, value));
